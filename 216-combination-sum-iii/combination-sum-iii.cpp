e37b4cc1dbabf8c546c95868d95728a58ff4d988
Amartya Raj <inevitablesmarty@gmail.com>
2026-01-11 23:12:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (79.01%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void func(vector<vector<int>>& ans,int k,int n, vector<int>& curr, int num){
+        if(n==0&&curr.size()==k&&num<=10){
+            ans.push_back(curr);
+            return;
+        }
+        if (num>9||curr.size()>k){
+            return;
+        }
+
+        for (int i=num;i<=n;i++){
+            curr.push_back(i);
+            func(ans,k,n-i,curr,i+1);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        func(ans,k,n,curr,1);
+        return ans;
+    }
+};
