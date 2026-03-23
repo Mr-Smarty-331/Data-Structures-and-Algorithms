b80619d02e655835a14e9e0acc31de988a6ae4ac
Amartya Raj <inevitablesmarty@gmail.com>
2026-03-23 19:52:56 +0530

    Time: 39 ms (6.77%) | Memory: 39.5 MB (9.96%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -3,14 +3,18 @@ public:
     int numSubarraysWithSum(vector<int>& nums, int k) {
         map<int,int> mp;
         int count=0;
+
         mp[0]=1;
         int sum=0;
-        if (nums.size()==1&&k!=nums[0]) return 0;
-        for (int x:nums){
+        if (nums.size()==1&&nums[0]!=k) return 0;
+        for(int x:nums) {
             sum+=x;
-            if (mp.contains(sum-k)) count+=mp[sum-k];
+            if(mp.contains(sum-k)) {
+                count+=mp[sum-k];
+            }
             mp[sum]++;
         }
+
         return count;
     }
 };
