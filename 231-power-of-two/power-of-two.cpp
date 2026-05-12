df1e21e1f31f52c18e578f79b422a9a9d6051452
Amartya Raj <inevitablesmarty@gmail.com>
2026-05-12 19:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (48.59%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    bool isPowerOfTwo(long long n) {
-        if (n==0) return false;
+    bool isPowerOfTwo(int n) {
+        if (n<=0) return false;
 
         if ( (n&(n-1)) == 0 ) return true;
 
