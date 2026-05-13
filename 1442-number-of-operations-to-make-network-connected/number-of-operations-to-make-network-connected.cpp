63c9d55e15249462e6f0423805989dfb5d6a8e85
Amartya Raj <inevitablesmarty@gmail.com>
2026-05-13 20:44:40 +0530

    Time: 33 ms (20.13%) | Memory: 48.7 MB (27.44%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -5,13 +5,19 @@ class Solution {
         return parent[u] = find(parent[u],parent);
     }
 
-    void unite(int u, int v,vector<int>& parent){
+    void unite(int u, int v,vector<int>& parent,vector<int>& size){
         int pu=find(u,parent);
         int pv=find(v,parent);
 
         if (pv == pu) return;
-
-        parent[pv] = pu;
+        if (size[pu] >= size[pv]) {
+            parent[pv] = pu;
+            size[pu]+=size[pv];
+        }
+        else {
+            parent[pu] = pv;
+            size[pv]+=size[pu];
+        }
     }
 
 public:
@@ -22,12 +28,14 @@ public:
         // simply the number of connected component-1
 
         vector<int> parent(n);
+        vector<int> size(n,1);
+
         for (int i=0;i<n;i++) {
             parent[i] = i;
         }
 
         for (auto e : edges) {
-            unite(e[0],e[1],parent);
+            unite(e[0],e[1],parent,size);
         }
 
         set<int> st;
