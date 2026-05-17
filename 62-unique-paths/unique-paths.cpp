f0facc53b359c1694dfedfa1587a7b6aa59b0c6d
Amartya Raj <inevitablesmarty@gmail.com>
2026-05-17 22:00:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.02%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,22 +1,37 @@
 class Solution {
-    int f(int i,int j,vector<vector<int>>& dp){
-        int m = dp.size();
-        int n = dp[0].size();
-        
-        if (i==m-1 && j==n-1) return 1;
-        else if (i>=m || j>=n) {
-            return 0;
-        }
+    // int f(int i,int j,vector<vector<int>>& dp){
+    //     int m = dp.size();
+    //     int n = dp[0].size();
 
-        if (dp[i][j]!= -1) return dp[i][j];
+    //     if (i==m-1 && j==n-1) return 1;
+    //     else if (i>=m || j>=n) {
+    //         return 0;
+    //     }
 
-        return dp[i][j] = f(i+1,j,dp) + f(i,j+1,dp);
-    }
+    //     if (dp[i][j]!= -1) return dp[i][j];
+
+    //     return dp[i][j] = f(i+1,j,dp) + f(i,j+1,dp);
+    // }
 
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[0][0] = 1;
+        for(int i = 0;i<m;i++) {
+            for(int j =0;j<n;j++) {
+                if (i==0 && j==0){continue;}
+                else {
+                    int left = 0;
+                    if (j>=1) left = dp[i][j-1];
+                    int up = 0;
+                    if (i>=1) up = dp[i-1][j];
+                    dp[i][j] = left + up;
+                }
+            }
+        }
 
-        return f(0,0,dp);
+        return dp[m-1][n-1];
+
+        // return f(0,0,dp);
     }
 };
