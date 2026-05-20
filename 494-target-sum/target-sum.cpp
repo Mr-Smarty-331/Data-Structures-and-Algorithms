ec54a38a92246f40f164c308de21120fde0539e8
Amartya Raj <inevitablesmarty@gmail.com>
2026-05-20 19:20:26 +0530

    Time: 13 ms (53.65%) | Memory: 25.3 MB (29.53%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,21 +1,31 @@
 class Solution {
-    int recur(int ind,int cur,vector<int>& nums) {
+    int recur(int ind,int cur,vector<int>& nums,vector<vector<int>>& dp,int sum) {
+        if (abs(cur) > sum)
+            return 0;
         if (ind == 0) {
             if (nums[0] == 0 && cur == 0) return 2;
             if (nums[0]==cur || (cur == -nums[0])) return 1;
             return 0;
         }
-        // adding
-        int ad = recur(ind-1,cur+nums[ind],nums);
-        int sub = recur(ind-1,cur-nums[ind],nums);
+        int shift = cur + sum;
+        if (dp[ind][shift] != -1) return dp[ind][shift];
 
-        return ad+sub;
+        int ad = recur(ind-1,cur+nums[ind],nums,dp,sum);
+        int sub = recur(ind-1,cur-nums[ind],nums,dp,sum);
+
+        return dp[ind][shift] = ad+sub;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         // firse wahi pick not pick -- two choice approach
         int n = nums.size();
-        if(n==1 && (nums[0]==0 && target==0)) return 2;
-        return recur(n-1,target,nums);
+        int sum = accumulate(nums.begin(),nums.end(),0);
+
+        if (abs(target) > sum) return 0;
+
+        int s = 2*sum+1;
+        vector<vector<int>> dp(n,vector<int>(s,-1));
+
+        return recur(n-1,target,nums,dp,sum);
     }
 };
