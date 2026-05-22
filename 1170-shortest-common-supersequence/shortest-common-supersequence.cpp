986430cebf93c2b538cf3205d0500185d9dc4e9b
Amartya Raj <inevitablesmarty@gmail.com>
2026-05-22 17:49:25 +0530

    Time: 26 ms (52.75%) | Memory: 26.7 MB (88.82%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        string str ="";
+        
+        int i = n,j = m;
+
+        while (i>0 && j>0){
+            if (s1[i-1]==s2[j-1]){
+                str += s1[i-1]; 
+                i--;
+                j--;
+            }
+
+            else{
+                if(dp[i][j-1]>=dp[i-1][j]){
+                    str += s2[j-1];
+                    j--;
+                }
+                else{
+                    str += s1[i-1];
+                    i--;
+                }
+            }
+        }
+
+        while(i>0) {
+            str+=s1[i-1];
+            i--;
+        }
+        while(j>0) {
+            str+=s2[j-1];
+            j--;
+        }
+
+        reverse(str.begin(),str.end());
+
+        return str;
+    }
+};
