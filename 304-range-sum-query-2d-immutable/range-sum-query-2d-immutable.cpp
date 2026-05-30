bfaac36121f50ec8010262917369bdae3e3bf627
Amartya Raj <inevitablesmarty@gmail.com>
2026-05-30 23:11:08 +0530

    Time: 43 ms (13.48%) | Memory: 152.2 MB (5.97%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -6,25 +6,31 @@ public:
     NumMatrix(vector<vector<int>>& matrix) {
         n = matrix.size();
         m = matrix[0].size();
-        dp.assign(n,vector<int>(m,0));
+        dp.assign(n+1,vector<int>(m+1,0));
 
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if (j == 0) {dp[i][j] = matrix[i][0];continue;}
-                dp[i][j] += matrix[i][j] + dp[i][j-1];
+        for(int i = 1;i<=n;i++){
+            vector<int> cur(m+1,0);
+            for(int j = 1;j<=m;j++){
+                cur[j] = cur[j-1]+matrix[i-1][j-1];
+                dp[i][j] = cur[j] + dp[i-1][j];
             }
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans = 0;
+        row1++;
+        row2++;
+        col1++;
+        col2++;
+        int main = dp[row2][col2];
+        int sub1 = 0;
+        if (row1>=1) sub1 = dp[row1-1][col2];
+        int sub2 = 0;
+        if (col1>=1) sub2 = dp[row2][col1-1];
+        int add = 0;
+        if (row1>=1 && col1>=1) add = dp[row1-1][col1-1];
 
-        for(int r = row1;r<=row2;r++){
-            int sub = 0;
-            if (col1!=0) sub = dp[r][col1-1];
-            ans += dp[r][col2] - sub;
-        }
-        return ans;
+        return main-sub1-sub2+add;
     }
 };
 
