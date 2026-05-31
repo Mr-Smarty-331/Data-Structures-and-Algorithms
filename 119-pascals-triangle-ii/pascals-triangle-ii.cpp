33638012a20850604545e5ee9564b0502fe6f67c
Amartya Raj <inevitablesmarty@gmail.com>
2026-06-01 00:58:28 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (48.92%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int n) {
+        vector<int> prev(1,1);
+        for(int i = 1;i<=n;i++){
+            vector<int> cur(i+1,1);
+            for(int j = 1;j<i;j++){
+                cur[j] = prev[j] + prev[j-1];
+            }
+            prev = cur;
+        }
+
+        return prev;
+    }
+};
