c3585e155aeaea821b488860b8b8ba3ae46f81c5
Amartya Raj <inevitablesmarty@gmail.com>
2026-05-31 15:13:02 +0530

    Time: 104 ms (47.74%) | Memory: 103.3 MB (41.55%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -24,10 +24,38 @@ class Solution {
 
         return dp[ind][buy] = profit;
     }
+
+    int tab(vector<int>& cost, int fee){
+        int n = cost.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        // int n = cost.size();
+        for(int ind = n-1;ind>=0;ind--){
+            for(int buy = 1;buy>=0;buy--){
+                int profit = 0;
+                if (buy) {
+                    int buy_now = -(cost[ind] + fee) + dp[ind+1][0];
+                    int buy_later = 0 + dp[ind+1][1];
+
+                    profit = max(buy_now,buy_later);
+                }
+                else{
+                    int sell_now = cost[ind] + dp[ind+1][1];
+                    int sell_later = 0 + dp[ind+1][0];
+
+                    profit = max(sell_now,sell_later);       
+                }
+
+                dp[ind][buy] = profit;                
+            }
+        }
+        return (dp[0][1]);
+    }
+
 public:
     int maxProfit(vector<int>& cost, int fee) {
         int n = cost.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return f(0,1,cost,fee,dp);
+        // vector<vector<int>> dp(n,vector<int>(2,-1));
+        // return f(0,1,cost,fee,dp);
+        return tab(cost,fee);
     }
 };
