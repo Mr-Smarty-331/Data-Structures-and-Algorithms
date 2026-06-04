965cd46785000ef52fc12c66dbb9a9825bd6574e
Amartya Raj <inevitablesmarty@gmail.com>
2026-06-05 00:00:46 +0530

    Time: 70 ms (7.21%) | Memory: 50.8 MB (5.07%) - LeetSync

--- 868-push-dominoes/push-dominoes.cpp
+++ 868-push-dominoes/push-dominoes.cpp
@@ -3,14 +3,12 @@ class Solution {
     // if the state has been vis before we dont tamper it,
     // we compare and save the earliest dirn
 
-    void push(priority_queue<pair<int,pair<int,int>>,
-            vector<pair<int,pair<int,int>>>, 
-            greater<>>& pq,vector<pair<int,int>>& ans){
+    void push(queue<pair<int,pair<int,int>>>& q,vector<pair<int,int>>& ans){
         int n = ans.size();
-        auto [time,p] = pq.top();
+        auto [time,p] = q.front();
         int ind = p.first;
         int dir = p.second;
-        pq.pop();
+        q.pop();
 
         if(ind == n||ind==-1) return;
 
@@ -20,23 +18,24 @@ class Solution {
         }
         else{
             ans[ind] = {time,dir};
-            pq.push({time+1,{ind+dir,dir}});
+            q.push({time+1,{ind+dir,dir}});
         }
     }
 
 public:
     string pushDominoes(string& str) {
         int n = str.size();
-        priority_queue<pair<int,pair<int,int>>,
-            vector<pair<int,pair<int,int>>>, 
-            greater<>> pq;
+        // priority_queue<pair<int,pair<int,int>>,
+        //     vector<pair<int,pair<int,int>>>, 
+        //     greater<>> pq;
+        queue<pair<int,pair<int,int>>> q;
         vector<pair<int,int>> ans(n,{-1,0}); //time , dir
         for(int i = 0;i<n;i++){
-            if(str[i]=='R') pq.push({0,{i,1}});
-            else if(str[i]=='L') pq.push({0,{i,-1}});
+            if(str[i]=='R') q.push({0,{i,1}});
+            else if(str[i]=='L') q.push({0,{i,-1}});
         }
-        while(!pq.empty()){
-            push(pq,ans);
+        while(!q.empty()){
+            push(q,ans);
         }
         string s(n,'.');
         for(int i = 0;i<n;i++){
