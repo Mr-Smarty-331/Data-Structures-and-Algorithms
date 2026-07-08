d21d887c91b279b28327e2087670c6ee71b0523f
Amartya Raj <inevitablesmarty@gmail.com>
2026-07-08 19:35:14 +0530

    Time: 2 ms (36.29%) | Memory: 10.9 MB (27.78%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    vector<vector<int>> ans;
+    void bfs(vector<int> &nums, vector<int> &cur,vector<int>& vis){
+        int n = nums.size();
+
+        if(cur.size()==n){
+            ans.push_back(cur);
+        }
+
+        for(int i = 0;i<n;i++){
+            if(vis[i]) continue;
+            vis[i] = 1;
+            cur.push_back(nums[i]);
+
+            bfs(nums,cur,vis);
+
+            vis[i] = 0;
+            cur.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> vis(n,0);
+        vector<int> cur;
+        bfs(nums,cur,vis);
+
+        return ans;
+    }
+};
